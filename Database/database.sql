9cb187131792b73d6f66b9f563c59c37fc117c89
Ammaidsahu <160928201+Ammaidsahu@users.noreply.github.com>
2024-05-05 21:08:32 +0500

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -1,30 +1,31 @@
-CREATE DATABASE IF NOT EXISTS Ambassador_Sports_Database;
-USE Ambassador_Sports_Database;
-CREATE TABLE  Products (
+CREATE DATABASE Ambassador_Sports;
+USE Ambassador_Sports;
+CREATE TABLE Football (
     P_ID INT AUTO_INCREMENT PRIMARY KEY,
     P_name VARCHAR(255),
     Size VARCHAR(50),
-    Material VARCHAR(100),
+    Material VARCHAR(50),
     Colour VARCHAR(50),
     Description TEXT,
-    Picture BLOB
+    Picture VARCHAR(255)
 );
-CREATE TABLE  Maker (
+CREATE TABLE IF NOT EXISTS Football_Maker (
     M_ID INT AUTO_INCREMENT PRIMARY KEY,
     M_name VARCHAR(255),
-    Address VARCHAR(255),
+    Address TEXT,
     CNIC VARCHAR(15),
     Contact_no VARCHAR(15),
-    Picture BLOB
+    Picture VARCHAR(255)
 );
-CREATE TABLE Order_History (
+CREATE TABLE IF NOT EXISTS Football_Order_History (
     P_ID INT,
     M_ID INT,
-    Total_Quantity_of_products INT,
+    Total_Quantity INT,
     Quantity_Delivered INT,
-    Quantity_Pending INT,
+    Quantity_Pending INT GENERATED ALWAYS AS (Total_Quantity - Quantity_Delivered) VIRTUAL,
     Price_Per_Item DECIMAL(10,2),
-    Total_Price DECIMAL(10,2),
-    FOREIGN KEY (P_ID) REFERENCES Products(P_ID),
-    FOREIGN KEY (M_ID) REFERENCES Maker(M_ID)
+    Total_Price DECIMAL(10,2) GENERATED ALWAYS AS (Quantity_Delivered * Price_Per_Item) VIRTUAL,
+    FOREIGN KEY (P_ID) REFERENCES Football (P_ID),
+    FOREIGN KEY (M_ID) REFERENCES Football_Maker (M_ID)
 );
+
