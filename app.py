c926b575de0bbaed3053546a8186e74d6dcf6248
Hassaan Ali Bukhari <112207209+B3TA-BLOCKER@users.noreply.github.com>
2024-05-11 23:33:05 +0500

    Added functions

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from config import config
 
 app = Flask(__name__)
+
 
 def connect():
     connection = None
@@ -64,9 +65,9 @@ def fetch_data_Gloves():
         products = []
         for row in cursor.fetchall():
             maker = {
-                'name': row[0], 
-                'unit_address': row[1],  
-                'contact_no': row[2] 
+                'name': row[0],  # Replace 0 with the index of the 'name' column in your database table
+                'unit_address': row[1],  # Replace 1 with the index of the 'unit_address' column
+                'contact_no': row[2]  # Replace 2 with the index of the 'contact_no' column
             }
             products.append(maker)
 
@@ -98,9 +99,43 @@ def fetch_data():
         makers = []
         for row in cursor.fetchall():
             maker = {
-                'name': row[1], 
-                'unit_address': row[2], 
-                'contact_no': row[4] 
+                'name': row[1],  # Replace 0 with the index of the 'name' column in your database table
+                'unit_address': row[2],  # Replace 1 with the index of the 'unit_address' column
+                'contact_no': row[4]  # Replace 2 with the index of the 'contact_no' column
+            }
+            makers.append(maker)
+
+        # Close database connection
+        cursor.close()
+        conn.close()
+
+        return makers
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        print("Error connecting to the database:", e)
+        return None
+
+
+def product_fetch_data():
+    try:
+        # Get database connection settings from database.ini
+        params = config()
+
+        # Connect to the database
+        conn = psycopg2.connect(**params)
+
+        cursor = conn.cursor()
+
+        # Query the database to fetch all rows from the Gloves_Maker table
+        cursor.execute("SELECT * FROM Gloves")
+
+        # Fetch all rows and store them in a list of dictionaries
+        makers = []
+        for row in cursor.fetchall():
+            maker = {
+                'name': row[0],  # Replace 0 with the index of the 'name' column in your database table
+                'unit_address': row[1],  # Replace 1 with the index of the 'unit_address' column
+                'contact_no': row[2]  # Replace 2 with the index of the 'contact_no' column
             }
             makers.append(maker)
 
@@ -134,6 +169,28 @@ def insert_product(product_name, size, material, colour, description):
         conn.close()
 
 
+# Function to delete an item from the database
+def delete_item_from_database(item_id):
+    try:
+        # Get database connection settings from database.ini
+        params = config()
+
+        # Connect to the database
+        conn = psycopg2.connect(**params)
+
+        cursor = conn.cursor()
+        # Execute the DELETE statement
+        cursor.execute("DELETE FROM Gloves WHERE P_ID = %s", (item_id,))
+
+        # Commit the transaction
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        print("Error deleting item from the database:", e)
+    finally:
+        cursor.close()
+        conn.close()
+
 
 @app.route('/Home_Page', methods=['POST'])
 def button_click():
@@ -149,7 +206,7 @@ def button_click():
 @app.route('/gloves/products')
 def gloves_products():
     data = fetch_data_Gloves()
-    return render_template('Gloves_product_window.html',products=data)
+    return render_template('Gloves_product_window.html', products=data)
 
 
 @app.route('/Add_Product.html')
@@ -177,8 +234,20 @@ def save_product():
 
 @app.route('/Delete_Product.html')
 def delete_product():
-    # Render the Delete_Product.html template
-    return render_template('Delete_Product.html')
+    data = product_fetch_data()
+    return render_template('Delete_Product.html', Gloves=data)
+
+
+@app.route('/delete_item', methods=['POST','GET'])
+def delete_item():
+    if request.method == 'POST':
+        item_id = request.form['delete_item']
+        print(item_id)
+        delete_item_from_database(item_id)  # Call the function to delete the item from the database
+        return redirect(url_for('gloves_products'))  # Redirect to the Gloves_product_window.html page
+
+
+
 
 
 @app.route('/gloves/makers')
@@ -205,7 +274,6 @@ def football_makers():
 @app.route('/football/order_history')
 def football_order_history():
     return render_template('order_history_page.html')
-
 
 
 if __name__ == "__main__":
